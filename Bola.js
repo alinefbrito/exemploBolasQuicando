9fd736f871e5ad39036d41ff4a2a8749053bda20
Aline F. Brito <alinefbrito@gmail.com>
2022-05-30 21:37:59 -0300

    correções a atualizações

--- Bola.js
+++ Bola.js
@@ -1,54 +1,56 @@
 class Bola {
 
-    constructor(x, y, velX, velY, color, size) {
-       this.x = x;
-       this.y = y;
-       this.velX = velX;
-       this.velY = velY;
-       this.color = color;
-       this.size = size;
-    }
- 
-    draw() {
-       ctx.beginPath();
-       ctx.fillStyle = this.color;
-       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-       ctx.fill();
-    }
- 
-    update() {
-       if ((this.x + this.size) >= width) {
-          this.velX = -(this.velX);
-       }
- 
-       if ((this.x - this.size) <= 0) {
-          this.velX = -(this.velX);
-       }
- 
-       if ((this.y + this.size) >= height) {
-          this.velY = -(this.velY);
-       }
- 
-       if ((this.y - this.size) <= 0) {
-          this.velY = -(this.velY);
-       }
- 
-       this.x += this.velX;
-       this.y += this.velY;
-    }
- 
-    collisionDetect(Bolas) {
-       for (const bola of Bolas) {
-          if (!(this === bola)) {
-             const dx = this.x - bola.x;
-             const dy = this.y - bola.y;
-             const distance = Math.sqrt(dx * dx + dy * dy);
- 
-             if (distance < this.size + bola.size) {
-               ball.color = this.color = randomRGB();
-             }
-          }
-       }
-    }
- 
- }
+   constructor(x, y, velX, velY, color, size) {
+      this.x = x;
+      this.y = y;
+      this.velX = velX;
+      this.velY = velY;
+      this.color = color;
+      this.size = size;
+   }
+//desenha a bola na tela
+   draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+      ctx.fill();
+   }
+
+   //atualiza a posição da bola na tela
+
+   update() {
+      if ((this.x + this.size) >= width) {
+         this.velX = -(this.velX);
+      }
+
+      if ((this.x - this.size) <= 0) {
+         this.velX = -(this.velX);
+      }
+
+      if ((this.y + this.size) >= height) {
+         this.velY = -(this.velY);
+      }
+
+      if ((this.y - this.size) <= 0) {
+         this.velY = -(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+   }
+//veruifica a colisão entre as bolas
+   collisionDetect(Bolas) {
+      for (var bola of Bolas) {
+         if (!(this === bola)) {
+            const dx = this.x - bola.x;
+            const dy = this.y - bola.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + bola.size) {
+              bola= this.color = randomRGB();
+            }
+         }
+      }
+   }
+
+}
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// set up canvas
+// gerencia o Canvas
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
@@ -6,20 +6,20 @@ const ctx = canvas.getContext('2d');
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-// function to generate random number
+// gera um número aleatório
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random RGB color value
+// gera uma cor aleatória
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
 
-
+//define um vetor de bolas
 const bolas = [];
 
 while (bolas.length < 25) {
@@ -35,9 +35,11 @@ while (bolas.length < 25) {
       size
    );
 
+   //atualiza o vetor
   bolas.push(bola);
 }
 
+//realiza um loop em todas as bolas geradas
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
