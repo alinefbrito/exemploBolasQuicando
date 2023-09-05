0cca8dd9bdcec68cf25c2d26ea4dab93655829b5
Aline F. Brito <alinefbrito@gmail.com>
2023-09-05 12:14:39 -0300

    Alterações aula 25/08/2023

--- main.js
+++ main.js
@@ -3,13 +3,16 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
+const width = canvas.width = window.innerWidth -5;
+const height = canvas.height = window.innerHeight -5;
 
 // gera um número aleatório
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    rdn = Math.random() * (max - min + 1);
+    var r = Math.floor( rdn ) + min;
+    console.log(r);
+  return r;
 }
 
 // gera uma cor aleatória
@@ -19,35 +22,35 @@ function randomRGB() {
 }
 
 
-//define um vetor de bolas
-const bolas = [];
-
-while (bolas.length < 25) {
+//define um vetor de bolhas
+const bolhas = [];
+nbolhas = random(10, 100);
+while (bolhas.length < nbolhas) {
    const size = random(10,20);
-   const bola = new Bola(
-      // posição de sempre uma bola de distância
+   const bolha = new Bolha(
+      // posição de sempre uma bolha de distância
       // fora das bordas para evitar erros de desenho
       random(0 + size,width - size),
       random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
+      random(-6,6),
+      random(-6,6),
       randomRGB(),
       size
    );
 
    //atualiza o vetor
-  bolas.push(bola);
+  bolhas.push(bolha);
 }
 
-//realiza um loop em todas as bolas geradas
+//realiza um loop em todas as bolhas geradas
 function loop() {
    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
    ctx.fillRect(0, 0,  width, height);
 
-   for (const bola of bolas) {
-    bola.draw();
-    bola.update();
-    bola.collisionDetect(bolas);
+   for (const bolha of bolhas) {
+    bolha.draw();
+    bolha.update();
+    bolha.collisionDetect(bolhas);
    }
 
    requestAnimationFrame(loop);
